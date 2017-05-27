a1ce6bb2b58bbfb93b4c9a5835548e8b71b9f189
Claudio Mignanti <c.mignanti@gmail.com>
2017-05-27 18:09:17 +0200

    Add support for extra nodes using NODES_URL_FILE

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -x
 
+
+if [[ -n $NODES_URL_FILE ]]; then
+    wget $NODES_URL_FILE -O /tmp/nodes_file.txt
+    cat /tmp/nodes_file.txt | xargs npm install -g
+fi
+
 if [[ $AUTH_METHOD == "http" ]]; then
     echo "Using http based auth!"
 else
@@ -9,8 +15,5 @@ else
     grep 'PASS' settings.js && node /app/gen_password.js
 fi
 
-if [[ -n $NODERED_MODULES ]]; then
-	npm install -g $NODERED_MODULES
-fi
 
 node-red -s /app/settings.js
