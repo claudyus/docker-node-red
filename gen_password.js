bcde43ea74ac412a8da623ff2022a38b4e495685
Claudio Mignanti <c.mignanti@gmail.com>
2017-11-24 09:21:06 +0100

    utils: force regeneration of passwd

--- gen_password.js
+++ gen_password.js
@@ -3,13 +3,16 @@ var fs = require('fs')
 var randomstring = require("randomstring");
 
 /*
+Usage: node gen_password.js [-f]
+
 If .nodered/.passwd file not exists generate a random password
 and store it hashed inside this file.
+The file is used by settings_passwd.js if AUTH_METHOD is set to `passwd`.
 
-The file is used by settings_passwd.js if AUTH_METHOD is set to passwd.
+Use -f options to force generation of new password if .passwd file exists.
 */
 
-if (fs.existsSync('.nodered/.passwd')) {
+if (fs.existsSync('.nodered/.passwd') && process.argv.indexOf('-f') == -1) {
   console.log('Password already generated, skipping...')
   return 0
 }
