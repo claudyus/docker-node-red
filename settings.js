claudyus/docker-node-red

commit 4a9433337e3bb82fc6d891b2071e6ef0b26922ed

Fix "noauth" use cases/title

settings.js
@@ -1,18 +1,24 @@
-module.exports = {
-  uiPort: process.env.PORT || 5000,
-  userDir: ".nodered/",
-  flowFile: "flows.json",
-  adminAuth: require('./settings_' + process.env.AUTH_METHOD + '.js'),
-  editorTheme: {
-    page: {
-      css: "/app/css/node-red.css"
-    },
-    header: {
-        "title": "Skyfield Machine",
-        "image": null,
-        "url": "#"
+module.exports = function () {
+
+  var obj = {
+    uiPort: process.env.PORT || 5000,
+    userDir: ".nodered/",
+    flowFile: "flows.json",
+    editorTheme: {
+      page: {
+        css: "/app/css/node-red.css"
+      },
+      header: {
+          "title": "Docker node-red",
+          "image": null,
+          "url": "#"
+      }
     }
   }
+  if (process.env.AUTH_METHOD != 'noauth')
+    obj['adminAuth']= require('./settings_' + process.env.AUTH_METHOD + '.js')
+
+  return obj
 }
 
 

css/node-red.css
@@ -1,3 +1,3 @@
 #header {
-    background: #f8f9fa
+    background: black;
 }

settings_noauth.js
@@ -1,2 +1,0 @@
-module.exports = {
-}

commit 1ab01fb1fe800593024ea58d75c560ff3924dbb0

Fix bug introduced in 4a9433337e3

settings.js
@@ -1,25 +1,17 @@
-module.exports = function () {
-
-  var obj = {
-    uiPort: process.env.PORT || 5000,
-    userDir: ".nodered/",
-    flowFile: "flows.json",
-    credentialSecret: process.env.SECRET || false,
-    editorTheme: {
-      page: {
-        css: "/app/css/node-red.css"
-      },
-      header: {
-          "title": process.env.TITLE || "Docker node-red",
-          "image": null,
-          "url": "#"
-      }
+module.exports =  {
+  uiPort: process.env.PORT || 5000,
+  userDir: ".nodered/",
+  flowFile: "flows.json",
+  credentialSecret: process.env.SECRET || false,
+  adminAuth: process.env.AUTH_METHOD ? require('./settings_' + process.env.AUTH_METHOD + '.js') : require('./settings.js'),
+  editorTheme: {
+    page: {
+      css: "/app/css/node-red.css"
+    },
+    header: {
+        "title": process.env.TITLE || "Docker node-red",
+        "image": null,
+        "url": "#"
     }
   }
-  if (process.env.AUTH_METHOD && process.env.AUTH_METHOD !== 'noauth')
-    obj['adminAuth']= require('./settings_' + process.env.AUTH_METHOD + '.js')
-
-  return obj
 }
-
-
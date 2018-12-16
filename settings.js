1ab01fb1fe800593024ea58d75c560ff3924dbb0
Claudio Mignanti <c.mignanti@gmail.com>
2018-12-16 11:46:13 +0100

    Fix bug introduced in 4a9433337e3

--- settings.js
+++ settings.js
@@ -1,25 +1,17 @@
-module.exports = function () {
-
-  var obj = {
-    uiPort: process.env.PORT || 5000,
-    userDir: ".nodered/",
-    flowFile: "flows.json",
-    credentialSecret: process.env.SECRET || false,
-    editorTheme: {
-      page: {
-        css: "/app/css/node-red.css"
-      },
-      header: {
-          "title": process.env.TITLE || "Docker node-red",
-          "image": null,
-          "url": "#"
-      }
+module.exports =  {
+  uiPort: process.env.PORT || 5000,
+  userDir: ".nodered/",
+  flowFile: "flows.json",
+  credentialSecret: process.env.SECRET || false,
+  adminAuth: process.env.AUTH_METHOD ? require('./settings_' + process.env.AUTH_METHOD + '.js') : require('./settings.js'),
+  editorTheme: {
+    page: {
+      css: "/app/css/node-red.css"
+    },
+    header: {
+        "title": process.env.TITLE || "Docker node-red",
+        "image": null,
+        "url": "#"
     }
   }
-  if (process.env.AUTH_METHOD && process.env.AUTH_METHOD !== 'noauth')
-    obj['adminAuth']= require('./settings_' + process.env.AUTH_METHOD + '.js')
-
-  return obj
 }
-
-
